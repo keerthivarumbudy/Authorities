f3728a7c955c5f5e50966d78ba3be4d53d5a1e80
Kushagra <vatsa.kushagra@gmail.com>
2018-01-29 08:51:56 +0530

    logout for authority done

--- app/src/main/java/com/example/android/authorities/AuthorityMenu.java
+++ app/src/main/java/com/example/android/authorities/AuthorityMenu.java
@@ -3,6 +3,9 @@ package com.example.android.authorities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +15,7 @@ public class AuthorityMenu extends AppCompatActivity {
     private Button buttonResults;
     private Button buttonUpdate;
     private String username;
+    private Toolbar mToolbar;
 
 
     @Override
@@ -30,5 +34,35 @@ public class AuthorityMenu extends AppCompatActivity {
 
             }
         });
+
+        mToolbar=(Toolbar)findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Authority");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() ==R.id.main_logout_button){
+                Intent i = new Intent(AuthorityMenu.this,MainActivity.class);
+                startActivity(i);
+        }
+
+        return true;
     }
 }
